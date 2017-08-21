dfcf0e1f6c8b4ce8a3a7936e5bbe420724f64650
chenming <294225707@qq.com>
2017-08-21 18:39:45 +0800

    优化blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,19 +8,32 @@ class PostController extends Controller
 {
     //列表页面
 	public function index(){
-		return view('post/index');
+		$posts =[
+			['title' => 'this is title1'],
+			['title' => 'this is title2'],
+			['title' => 'this is title3'],
+		];
+		$topics = [];
+		//
+		return view('post/index',compact('posts','topics'));
 	}
 
 	//详情页面
-	public function show(){}
+	public function show(){
+		return view('post/show');
+	}
 
 	//创建页面
-	public function create(){}
+	public function create(){
+		return view('post/create');
+	}
 	//具体的创建逻辑
 	public function store(){}
 
 	//edit
-	public function edit(){}
+	public function edit(){
+		return view('post/edit');
+	}
 
 	//编辑的逻辑
 	public function update(){}
---
+++ app/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //模型默认 会zhi
+}
